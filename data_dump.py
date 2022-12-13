09103adb6ab16940ea9a763e0495e069594ef3c4
Ankita <acchaudhari26@gmail.com>
2022-12-13 20:55:01 +0530

    adding all files

---
+++ data_dump.py
@@ -0,0 +1,24 @@
+import pymongo
+import pandas as pd
+import json
+
+# Provide the mongodb localhost url to connect python to mongodb.
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+
+
+DATA_FILE_PATH ="/config/workspace/aps_failure_training_set1.csv"
+DATABASE_NAME="aps"
+COLLECTION_NAME="sensor"
+
+if __name__=="__main__":
+    df=pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and Columns are:{df.shape}")
+
+# In mongodb we require data in json format so we convert dataframe to json format 
+df.reset_index(drop=True,inplace=True)
+json_record= list(json.loads(df.T.to_json()).values())
+print(json_record[0])
+
+# we get json format data then insert data into mongodb
+client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+
--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-import pymongo
+import sys,os
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+def test_logger_and_exception():
+     try:
+          logging.info("starting the test_logger_and_exception")
+          result=3/0
+          print(result)
+          logging.info("stopping the test_logger_and_exception")
 
-# Database Name
-dataBase = client["neurolabDB"]
+     except Exception as e:
+          logging.debug(str(e))
+          logging.debug("stopping the test_logger_and_exception") 
+          raise SensorException(e,sys) 
 
-# Collection  Name
-collection = dataBase['Products']
 
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
+     if __name__=="__main__":
+          try:
+               test_logger_and_exception()
+          except Exception as e:
+               print(e)
 
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+         
